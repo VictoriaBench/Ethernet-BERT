6399db755ee3e3c46a0313161ec4cc24f59e1a6c
Blake-Denham <blake.denham@gmail.com>
2021-10-01 09:01:45 +0000

    bert and data gen changes

--- data_generator.py
+++ data_generator.py
@@ -12,7 +12,7 @@ class DataGenerator:
         self.bufferSize = bufferSize
         self.buffer = io.BytesIO()
         for i in range(self.bufferSize):
-            self.buffer.write(self.getPacketFromID(i))
+            self.buffer.write(self.generatePacketFromID(i))
         self.buffer.seek(0)
 
    
@@ -35,7 +35,7 @@ class DataGenerator:
             raise StopIteration
 
 
-    def getPacketFromID(self, id:int):
+    def generatePacketFromID(self, id:int):
         if self.packetSize == 0:
             return b''
         rand = random.Random(id % self.numPackets)
@@ -45,6 +45,13 @@ class DataGenerator:
         result = header + integer.to_bytes(self.packetSize - 12, sys.byteorder)
         return result
 
+    def getPacketFromID(self, id:int):
+        prev = self.buffer.tell()
+        self.buffer.seek((id % self.bufferSize)* self.packetSize)
+        result = bytearray(self.buffer.read(self.packetSize))
+        result[:12] = self.nextID.to_bytes(4, byteorder=sys.byteorder)*3
+        self.buffer.seek(prev)
+        return result
 
 def decodePacket(data):
     ids = [int.from_bytes(data[:4], sys.byteorder), int.from_bytes(data[4:8], sys.byteorder), int.from_bytes(data[8:12], sys.byteorder)] 
